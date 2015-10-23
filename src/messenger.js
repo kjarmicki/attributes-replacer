2ae38a258ca0c68bee9b703d361719ee6b248495
Krystian Jarmicki <jarmicki@gmail.com>
2015-10-23 20:40:28 +0200

    messenger rewritten

--- src/messenger.js
+++ src/messenger.js
@@ -1,24 +1,15 @@
-'use strict';
+/* global chrome */
 
-const NAME = 'attr-replacer';
+'use strict';
 
 let messenger = {
     send: function(content) {
-        let post = Object.assign({}, {
-            from: NAME
-        }, content);
-
-        window.postMessage(post, '*');
+        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+            chrome.tabs.sendMessage(tabs[0].id, content);
+        });
     },
     listen: function(handler) {
-        window.addEventListener('message', event => {
-            if(event.source !== window) return;
-
-            let data = event.data;
-            if(data.from && data.from === NAME) {
-                handler(data);
-            }
-        });
+        chrome.runtime.onMessage.addListener(handler);
     }
 };
 
