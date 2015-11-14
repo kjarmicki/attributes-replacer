6f8cf8a06b4e8faf4b7ee40897ea0e8d1a7d5f01
Krystian Jarmicki <jarmicki@gmail.com>
2015-11-14 16:50:10 +0100

    build clean

--- create-manifest.js
+++ create-manifest.js
@@ -4,7 +4,7 @@ let packageJSON = require('./package.json');
 let manifest = require('./src/manifest');
 
 manifest.createAsFileSync('dist/manifest.json', {
-    name: packageJSON.name,
+    name: packageJSON.name.replace('-', ' '),
     description: packageJSON.description,
     version: packageJSON.version
 });
