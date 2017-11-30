0b17ac93dce9435bf42ba4a0a2118e473aaf695f
lucaspetter <lucaspetter@users.noreply.github.com>
2017-11-29 19:24:32 -0800

    Allow attributes with numbers or capital letters

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@
 module.exports = {
     extractAttributeFromSelector(selector) {
         try {
-            return selector.match(/\[([a-z\-]+)\]/)[1];
+            return selector.match(/\[([a-zA-Z0-9\-]+)\]/)[1];
         }
         catch(e) {
             throw new Error('Error while parsing attribute from selector '  + selector);
